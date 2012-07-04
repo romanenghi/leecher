f72da3b8846c8882bc0cbeb16b7eb6f97609b4c9
unknown <Francesco@.(none)>
2012-07-04 23:59:02 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leecher::Application.config.secret_token = '72c8dd6781ccf60a0b461f9940726e60a50483a29a19bb72f3497cc9567abfc6454aad2d041eb410d74d125100e4abe4963cc423a66e2fab0b9b9b6fa9ffb27d'
